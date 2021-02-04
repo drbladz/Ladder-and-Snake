5bad76bdf65d9f6d507e784ddb102e0bda73ec5a
DivSam <48162524+DivSam@users.noreply.github.com>
2021-02-04 14:51:50 -0500

    player positioning is accurate

--- Main.java
+++ Main.java
@@ -2,7 +2,7 @@ import java.util.Arrays;
 import java.util.Scanner;
 
 public class Main {
-
+//stuff in main will prolly get moved to playgame methode, in main just to test for now
     public static void main(String[] args) {
         // write your code here
         Scanner kb = new Scanner(System.in);
@@ -15,8 +15,17 @@ public class Main {
         for (int i = 0; i < amountOfPlayers; i++) {
             playa[i] = new Players();
 
-            System.out.println(playa[i].flipDice());
+
         }
+        int diceRoll = playa[1].flipDice();
+        System.out.println("roll value is " + diceRoll);
+
+        System.out.println("position is " + playa[1].initialPosition(diceRoll));
+
+        int diceRoll2 = playa[1].flipDice();
+        System.out.println("new roll value is " + diceRoll2);
+
+        System.out.println("new position is " + playa[1].playerPosition(diceRoll2));
 
 
 
@@ -29,11 +38,11 @@ public class Main {
 class LadderAndSnake {
 
     int nbOfPlayers;
-
+    final int winner = 100;
     public LadderAndSnake(int nbOfPlayers) { //use the number of players to decide how the game loops, create a separate class for players
         this.nbOfPlayers = nbOfPlayers;
 
-        //THE GAME BOARD IN GOOD ORDER TOO xD
+        //THE GAME BOARD IN GOOD ORDER TOO xD, probably use this just for some sort of gui.
         int board[][] = new int[10][10];
         int maxNumber = 100;
         int reverseNum = 90;
@@ -74,12 +83,17 @@ class LadderAndSnake {
     public String toString() {
         return "";
     }
+    public void playGame(){
+
+    }
 
 }
 
 
 class Players {
     private static int count = 0;
+    private static int startPosition = 0;
+    private int PosAfter = 0;
 
     public Players() {
         count++;
@@ -89,8 +103,18 @@ class Players {
         System.out.println(count);
     }
     public int flipDice() {
+        int diceValue = (int) (Math.random() * 6) + 1;
+        return diceValue;
+    }
 
-        return (int) (Math.random() * 6) + 1;
+    public int initialPosition(int diceValue){
+        PosAfter = startPosition + diceValue;
+        return PosAfter;
+    }
+
+    public int playerPosition(int diceValue2){
+        int ppos = PosAfter + diceValue2;
+        return ppos;
     }
 
 }
