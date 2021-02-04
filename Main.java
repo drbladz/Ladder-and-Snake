7d32b1e8f9a5206ca0ddaf4b3b0aeb9d2d39977f
DivSam <48162524+DivSam@users.noreply.github.com>
2021-02-04 11:37:53 -0500

    added dice method and player class

--- Main.java
+++ Main.java
@@ -1,10 +1,26 @@
 import java.util.Arrays;
+import java.util.Scanner;
 
 public class Main {
 
     public static void main(String[] args) {
         // write your code here
-       LadderAndSnake yo = new LadderAndSnake(2);
+        Scanner kb = new Scanner(System.in);
+        System.out.println("How many players are playing today?: ");
+        int amountOfPlayers = kb.nextInt();
+        LadderAndSnake yo = new LadderAndSnake(amountOfPlayers);
+
+        Players[] playa = new Players[amountOfPlayers];
+
+        for (int i = 0; i < amountOfPlayers; i++) {
+            playa[i] = new Players();
+
+            System.out.println(playa[i].flipDice());
+        }
+
+
+
+
 
     }
 }
@@ -12,28 +28,28 @@ public class Main {
 
 class LadderAndSnake {
 
-int nbOfPlayers;
-    public LadderAndSnake(int nbOfPlayers) {
+    int nbOfPlayers;
+
+    public LadderAndSnake(int nbOfPlayers) { //use the number of players to decide how the game loops, create a separate class for players
         this.nbOfPlayers = nbOfPlayers;
 
         //THE GAME BOARD IN GOOD ORDER TOO xD
         int board[][] = new int[10][10];
         int maxNumber = 100;
         int reverseNum = 90;
-        for (int i = 0; i<10; i++){
-            if (i%2==0) {
+        for (int i = 0; i < 10; i++) {
+            if (i % 2 == 0) {
                 for (int j = 0; j < 10; j++) {
                     board[i][j] = maxNumber;
                     System.out.print(board[i][j] + "\t");
                     maxNumber--;
                     reverseNum++;
                 }
-            }
-            else{
-                reverseNum = maxNumber-9;
-                for(int j = 0; j<10; j++){
+            } else {
+                reverseNum = maxNumber - 9;
+                for (int j = 0; j < 10; j++) {
                     board[i][j] = reverseNum;
-                    System.out.print(board[i][j]+"\t");
+                    System.out.print(board[i][j] + "\t");
                     maxNumber--;
                     reverseNum++;
                 }
@@ -45,5 +61,36 @@ int nbOfPlayers;
     }
 
 
+
+    public int getNbOfPlayers() {
+        return nbOfPlayers;
+    }
+
+    public void setNbOfPlayers(int nbOfPlayers) {
+        this.nbOfPlayers = nbOfPlayers;
+    }
+
+
+    public String toString() {
+        return "";
+    }
+
 }
 
+
+class Players {
+    private static int count = 0;
+
+    public Players() {
+        count++;
+    }
+
+    public static void numberOfPlayers() {
+        System.out.println(count);
+    }
+    public int flipDice() {
+
+        return (int) (Math.random() * 6) + 1;
+    }
+
+}
